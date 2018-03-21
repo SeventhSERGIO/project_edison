0d8cd5dd432c4d7adac0310c819dce7bae18889c
Adrian Martinez <adrianmartinez@Adrians-MacBook-Pro.local>
2018-03-20 18:47:31 -0700

    Adrian

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def on_connect(client, userdata, flags, rc):
     print("Connected to server. Connection code: "+str(rc))
 
     # Do a subscribe to "MQTTEdison" topic whenever a new connection is done 
-    client.subscribe("MQTTEdison")
+    client.subscribe("test")
 
 # Callback - called when a new message is received / published
 def on_message(client, userdata, msg):
@@ -28,7 +28,7 @@ client = mqtt.Client()
 client.on_connect = on_connect   #configures callback for new connection established
 client.on_message = on_message   #configures callback for new message received
 
-client.connect("test.mosquitto.org", 1883, 60)  #connects to broker (the '60' parameter means keepalive time). If no messages are exchanged in 60 seconds, This program automatically sends ping to broker (for keeping connection on)
+client.connect("192.168.1.77", 1883, 60)  #connects to broker (the '60' parameter means keepalive time). If no messages are exchanged in 60 seconds, This program automatically sends ping to broker (for keeping connection on)
 
 #Stay here forever. Blocking function.
 client.loop_forever()
