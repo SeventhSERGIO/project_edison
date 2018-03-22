8b7de6907a9ce84e965186fafa3748bc9a5247dd
Adrian Martinez <adrianmartinez@Adrians-MacBook-Pro.local>
2018-03-21 22:47:20 -0700

    Adrian
    gpio_interrupt.c

---
+++ gpio_interrupt.c
@@ -0,0 +1,33 @@
+#include <signal.h>
+#include <mraa/gpio.h>
+
+sig_atomic_t volatile run_flag = 1;
+
+void do_when_interrupted(int sig)
+{
+	if (sig == SIGINT)
+		run_flag = 0;
+}
+
+int main()
+{
+	mraa_gpio_context led;
+
+	led = mraa_gpio_init(13);
+	button = mraa_gpio_init(4);
+	mraa_gpio_dir(led, MRAA_GPIO_OUT);
+	mraa_gpio_dir(button, MRAA_GPIO_IN);
+
+	mraa_gpio_isr(button, MRAA_GPIO_EDGE_RISING, &do_when_interrupted, NULL);
+
+	while (run_flag)
+	{
+		mraa_gpio_write(led, 1);
+		sleep(1);
+		mraa_gpio_write(led, 0);
+		sleep(1);
+	}
+
+	mraa_gpio_close(led);
+	return 0;
+}
