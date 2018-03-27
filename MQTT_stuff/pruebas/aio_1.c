8ee3102b9261a8dc030b00061215ec5a0e824ec5
Adrian Martinez <adrianmartinez@Adrians-MacBook-Pro.local>
2018-03-26 23:58:40 -0700

    Adrian

--- MQTT_stuff/pruebas/aio_1.c
+++ MQTT_stuff/pruebas/aio_1.c
@@ -1,11 +1,6 @@
-#include <signal.h>
-#include <stdlib.h>
-#include <unistd.h>
-/* mraa header */
-#include "mraa/aio.h"
-/* AIO port */
-#define AIO_PORT 0
-int main (void)
+#include "mqtt.h"
+
+int sensor (void)
 {
     mraa_result_t status = MRAA_SUCCESS;
     mraa_aio_context aio;
@@ -24,8 +19,7 @@ int main (void)
     }
     value = mraa_aio_read(aio);
     //float_value = mraa_aio_read_float(aio);
-    fprintf(stdout, "ADC A0 read %X - %d\n", value, value);
+    //fprintf(stdout, "ADC A0 read %X - %d\n", value, value);
     //fprintf(stdout, "ADC A0 read float - %.5f\n", float_value);
-
-    return EXIT_SUCCESS;
+    return value;
 }
