2eb9bafa1972fb7a6df69623cbf1a156c7fe46af
Adrian Martinez <adrianmartinez@Adrians-MacBook-Pro.local>
2018-03-27 13:59:32 -0700

    Adrian

--- MQTT_stuff/file.c
+++ MQTT_stuff/file.c
@@ -105,10 +105,8 @@ int sensor (void)
     mraa_aio_context aio;
     uint16_t value = 0;
 
-    /* initialize mraa for the platform (not needed most of the times) */
     mraa_init();
 
-    //! [Interesting]
     /* initialize AIO */
     aio = mraa_aio_init(AIO_PORT);
     if (aio == NULL) {
@@ -117,8 +115,6 @@ int sensor (void)
         return EXIT_FAILURE;
     }
     value = mraa_aio_read(aio);
-    //float_value = mraa_aio_read_float(aio);
-    //fprintf(stdout, "ADC A0 read %X - %d\n", value, value);
-    //fprintf(stdout, "ADC A0 read float - %.5f\n", float_value);
+
     return value;
 }
